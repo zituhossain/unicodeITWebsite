f60693989c4c4b44e223d6989d9bedd830710aba
Zitu Hossain <zituhossain@gmail.com>
2026-08-06 23:06:23 +0600

    remove aexo folder

--- aexo/next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  images: { formats: ["image/avif", "image/webp"] },
-  devIndicators: false,
-  poweredByHeader: false,
-};
-
-export default nextConfig;
